43ace8e76a23f9889ed5bbe5b412a95c9cf0bec5
Shubham Jain <shujain@splunk.com>
2019-01-29 17:09:15 -0800

    func tests cleanup

--- test/k8s_logging_tests/test_config.py
+++ test/k8s_logging_tests/test_config.py
@@ -36,12 +36,9 @@ def test_cluster_name(setup, test_input, expected):
     '''
     Clustername is a configurable parameter, test that user specified cluster-name is attached as a metadata to all the logs
     '''
-
     logging.getLogger().info("testing test_clusterName input={0} \
                 expected={1} event(s)".format(test_input, expected))
-
     search_query = "index=circleci_events cluster_name=circleci-k8s-cluster | head 1"
-
     events = check_events_from_splunk(start_time="-24h@h",
                                   url=setup["splunkd_url"],
                                   user=setup["splunk_user"],
